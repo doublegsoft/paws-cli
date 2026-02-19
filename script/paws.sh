46342871c0e82f9f4e7123d6dc6d6fac84dee0dc
Christian Gann <guo.guo.gan@gmail.com>
2026-02-19 18:47:49 +0800

    argparse support

--- script/paws.sh
+++
@@ -1,3 +0,0 @@
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:../libclix/build/darwin:../libpal/build/darwin:../libclix/3rd/gfc/build/darwin
-
-build/darwin/paws
--- src/mac/main.mm
+++ src/mac/main.mm
@@ -4,8 +4,17 @@
 
 #include <gfc.h>
 #include <libpal.hpp>
+#include <argparse.h>
+
+static const char *const usages[] = 
+{
+  "paws [options]",
+  NULL,
+};
 
 #define PAL_ROOT_DIR            "/Users/christian/Paws"
+
+static const char* pal_root_dir = NULL;
 
 class PawsHandler : public pal::InstructionHandler
 {
@@ -19,15 +28,15 @@ void handleOnClick(int x, int y, const char* imagePath, bool required, bool rela
     if (relative)
     {
       if (required)
-        [simulator clickAtOffsetX:x andY:y untilFound:[NSString stringWithFormat:@"%s/%s", PAL_ROOT_DIR, imagePath]];
+        [simulator clickAtOffsetX:x andY:y untilFound:[NSString stringWithFormat:@"%s/%s", pal_root_dir, imagePath]];
       else
-        [simulator clickAtOffsetX:x andY:y ifFound:[NSString stringWithFormat:@"%s/%s", PAL_ROOT_DIR, imagePath]];
+        [simulator clickAtOffsetX:x andY:y ifFound:[NSString stringWithFormat:@"%s/%s", pal_root_dir, imagePath]];
     }
     else
       if (required)
         ;// [simulator clickAtX:x andY:y untilSeen:[NSString stringWithFormat:@"%s/%s", PAL_ROOT_DIR, imagePath]];
       else
-        [simulator clickAtX:x andY:y ifFound:[NSString stringWithFormat:@"%s/%s", PAL_ROOT_DIR, imagePath]];
+        [simulator clickAtX:x andY:y ifFound:[NSString stringWithFormat:@"%s/%s", pal_root_dir, imagePath]];
   }
   else
     [simulator clickAtX:x andY:y];
@@ -53,7 +62,22 @@ void handleOnScroll(int offset, const char* direction, const char* path) overrid
 void handleOnSave(const char* path) override
 {
   NSString* templateString = [NSString stringWithUTF8String:path];
-  NSString* renderedPath = templateString;
+
+  NSString* filePath = templateString;
+  NSString *fileName = [[filePath lastPathComponent] stringByDeletingPathExtension];
+  NSString* extension = [filePath pathExtension];
+
+  NSCalendar* calendar = [NSCalendar currentCalendar];
+  NSDateComponents* components = [calendar components:(NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond) fromDate:[NSDate date]];
+  NSString* datetime = [NSString stringWithFormat:@"%04ld%02ld%02ld%02ld%02ld%02ld",
+                       (long)components.year,
+                       (long)components.month,
+                       (long)components.day,
+                       (long)components.hour,
+                       (long)components.minute,
+                       (long)components.second];
+
+  NSString* renderedPath = [NSString stringWithFormat:@"%@-%@.%@", fileName, datetime, extension];
   
   NSPasteboard* pasteboard = [NSPasteboard generalPasteboard];
   NSArray* classes = @[[NSString class], [NSAttributedString class], [NSURL class]];
@@ -65,7 +89,7 @@ void handleOnSave(const char* path) override
       if ([object isKindOfClass:[NSString class]]) {
         NSString* content = (NSString*) object;
         content = [content stringByAppendingString:@"\n"];
-        NSString* filePath = [NSString stringWithFormat:@"%s/%@", PAL_ROOT_DIR, renderedPath];
+        NSString* filePath = [NSString stringWithFormat:@"%s/%@", pal_root_dir, renderedPath];
         NSFileManager *fileManager = [NSFileManager defaultManager];
         if (![fileManager fileExistsAtPath:filePath]) {
           [fileManager createFileAtPath:filePath contents:nil attributes:nil];
@@ -99,16 +123,36 @@ ClixMacSimulator* simulator;
 int
 main(int argc, char* argv[])
 {
+  char* pal = NULL;
+  char* wd = NULL;
+
+  struct argparse_option options[] = {
+    OPT_HELP(),
+    OPT_STRING('p', "pal", &pal, "the PAL script path", NULL, 0, 0),
+    OPT_STRING('w', "work_directory", &wd, "the work directory", NULL, 0, 0),
+    OPT_END(),
+  };
+  struct argparse argparse;
+  argparse_init(&argparse, options, usages, 0);
+  argparse_describe(&argparse, "\nExecute PAL automation script.", NULL);
+  
+  argc = argparse_parse(&argparse, argc, (const char**) argv);
+  if (pal == NULL || wd == NULL) 
+  {
+    argparse_usage(&argparse);
+    return -1;
+  }
+  pal_root_dir = wd;
   @autoreleasepool {
     gfc_gc_init();
     
     PawsHandler handler;
-    handler.simulator = [[ClixMacSimulator alloc] initWithDirectory:@PAL_ROOT_DIR];
+    handler.simulator = [[ClixMacSimulator alloc] initWithDirectory:[NSString stringWithUTF8String:wd]];
     
     pal::Program prog(&handler);
     
     NSError* error = nil;
-    NSString* filePath = @PAL_ROOT_DIR "/paws.pal";
+    NSString* filePath = [NSString stringWithUTF8String:pal];
     NSString* fileContent = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:&error];
     
     prog.Evaluate([fileContent UTF8String], (int)fileContent.length);
